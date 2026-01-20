a7064734055c7e99cf14dfa676bbf84e84bffe6a
IEatChange <73599546+IEatChange@users.noreply.github.com>
2026-01-20 12:33:19 +0200

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 docker build -t ovmf-vbios-patch docker-build
-docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh gtx-1050-mobile.rom
+docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh alt-1050m.rom
 tar -czf build/ovmf-vbios-patched.tgz build/OVMF*
